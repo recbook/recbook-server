00ce68194d8b0c0b4f4802d4e50eb60e4424d4c9
Patrick Shim <nasuk0610@gmail.com>
2016-12-29 14:31:49 +0900

    add express gulp es6

---
+++ app/app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import config from 'config';
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+const port = process.env.PORT || config.PORT;
+app.listen(port, () =>{
+  console.log(`recbook listening on port ${port}!`);
+});
