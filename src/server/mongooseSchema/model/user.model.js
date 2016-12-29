0657f97669c254917eb2a3cacc6b0470a02f1f2a
Yangwoo Lee <lyw0149@gmail.com>
2016-12-29 17:17:58 +0900

    update travis.yml

--- src/server/mongooseSchema/model/user.model.js
+++ src/server/mongooseSchema/model/user.model.js
@@ -5,22 +5,16 @@ const User = mongoose.model('User', UserSchema);
 
 //set up mockup data
 User.remove().then(() => {
-  const users = [];
   for (let i = 0; i < 100; i++) {
     User.create(new User({
       name: `UserType22${i}`,
       password: `secretrererer`,
       email: `lyw22${i}@naver.com`,
-      phone: '+821062348149',
-      point: [(Math.random() - 0.5) * 360, (Math.random() - 0.5) * 180],
       createdAt: new Date(),
-      friends: users.map((i) => i._id),
     })).catch((err)=> {
       console.log(err);
     });
-
   }
-
 });
 
 export default User;
