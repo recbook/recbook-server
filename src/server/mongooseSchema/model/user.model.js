d414ebb2e5a74337cfcd95e98db9cdc27cd80b44
Yangwoo Lee <lyw0149@gmail.com>
2016-12-29 17:32:37 +0900

    delete useless code.

--- src/server/mongooseSchema/model/user.model.js
+++ src/server/mongooseSchema/model/user.model.js
@@ -3,18 +3,4 @@ import UserSchema from '../schema/user.schema';
 
 const User = mongoose.model('User', UserSchema);
 
-//set up mockup data
-User.remove().then(() => {
-  for (let i = 0; i < 100; i++) {
-    User.create(new User({
-      name: `UserType22${i}`,
-      password: `secretrererer`,
-      email: `lyw22${i}@naver.com`,
-      createdAt: new Date(),
-    })).catch((err)=> {
-      console.log(err);
-    });
-  }
-});
-
 export default User;
